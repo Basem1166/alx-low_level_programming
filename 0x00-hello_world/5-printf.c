b7dd826be61164f1d38b1ab4a5492597de0448b9
Basem <basem_elgalfy@yahoo.com>
2023-06-15 07:58:39 -0700

    c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int main(){
+/**
+ *  * main - Entry point of the program
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+
+int main()
+{
 	printf("with proper grammar, but the outcome is a piece of art,");
 	return 0;
 }
