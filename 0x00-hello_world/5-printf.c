ffba2a4874e2e0e56033cf9110bc1f71c45a638c
Basem <basem_elgalfy@yahoo.com>
2023-06-15 08:31:16 -0700

    c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,8 @@
 
 /**
 * main - Entry point of the program
-* printf - prints the statment "with proper grammar, but the outcome is a piece of art"
+* printf - prints the statment "with proper grammar,
+* but the outcome is a piece of art"
 * Return: Always 0 (Success)
 */
 int main(void)
