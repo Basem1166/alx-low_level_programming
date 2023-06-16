cf095d9f23d50696e7ed56182d5949e123f7e1f3
Basem <basem_elgalfy@yahoo.com>
2023-06-16 09:53:28 -0700

    i hate betty

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,22 +3,21 @@
 #include <stdio.h>
 
 /**
-*  betty style doc for function main goes there 
+*  This program will assign a random number to the variable n each time it is executed.
 */
 int main(void)
-{
-		int n;
-
-			srand(time(0));
-				n = rand() - RAND_MAX / 2;
+{	
+	int n;	
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 					
-					if (n > 0)
-								printf("%d is positive\n", n);
-						else if (n < 0)
-									printf("%d is negative\n", n);
-							else
-										printf("%d is zero\n", n);
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
 								
-								return (0);
+	return (0);
 }
 
