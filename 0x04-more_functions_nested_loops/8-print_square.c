3f57fa00a2485f06d48e37598ed3e4d64704b055
Basem <basem_elgalfy@yahoo.com>
2023-06-22 15:52:57 -0700

    0x04

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,9 +7,9 @@
 void print_square(int size)
 {
 
-	int row , col;
+	int row, col;
 
-	for (row =1; row <= size; row++)
+	for (row = 1; row <= size; row++)
 	{
 		for (col = 1; col <= size; col++)
 		{
