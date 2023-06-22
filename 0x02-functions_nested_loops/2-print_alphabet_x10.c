b2f22d5443d9a5b134559312beb21dd850599910
Basem <basem_elgalfy@yahoo.com>
2023-06-21 17:22:31 -0700

    0x02

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,10 +6,10 @@
  */
 void print_alphabet_x10(void)
 {
-	char letter;
+	
 	for(int i = 0 ; i<10 ; i++)
 	{
-		for (letter = 'a'; letter <= 'z'; letter++)
+		for (char letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
 	
 
