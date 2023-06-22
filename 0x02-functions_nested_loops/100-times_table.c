78b3218622e775884d222bc473e1f1c482ca1e6e
Basem <basem_elgalfy@yahoo.com>
2023-06-21 18:41:55 -0700

    0x02

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -23,24 +23,32 @@ void print_times_table(int n)
 					_putchar(',');
 					_putchar(' ');
 
-					if (result < 10)
+					if (result < 100)
 					{
 						_putchar(' ');
-						_putchar(' ');
-					}
-					else if (result >= 10 && result < 100)
-					{
-						_putchar(' ');
+						if (result < 10)
+							_putchar(' ');
 					}
 				}
 
-				_putchar((result / 100) + '0');
-				_putchar(((result / 10) % 10) + '0');
-				_putchar((result % 10) + '0');
+				if (result < 10)
+				{
+					_putchar(result + '0');
+				}
+				else if (result >= 10 && result < 100)
+				{
+					_putchar((result / 10) + '0');
+					_putchar((result % 10) + '0');
+				}
+				else
+				{
+					_putchar((result / 100) + '0');
+					_putchar(((result / 10) % 10) + '0');
+					_putchar((result % 10) + '0');
+				}
 			}
 
 			_putchar('\n');
 		}
 	}
 }
-
