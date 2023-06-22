773329a574ff18c6eb9d674e612c2ea4e22c1c13
Basem <basem_elgalfy@yahoo.com>
2023-06-21 17:20:01 -0700

    0x02

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Prints the alphabet in lowercase, followed by a new line.
+ * print_alphabet_x10 - Prints the alphabet in lowercase, followed by a new line
+ * @c: The character to be checked..
  */
 void print_alphabet_x10(void)
 {
