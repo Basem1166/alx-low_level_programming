0a383f8be3a1841cd517f19f693303bee4ad51ed
Basem <basem_elgalfy@yahoo.com>
2023-06-22 16:32:35 -0700

    0x04

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *
  * Return: Always 0
  */
 int main(void)
@@ -18,8 +17,7 @@ int main(void)
 			printf("Buzz ");
 		else
 			printf("%d ", i);
-
 	}
+	printf('\n');
 	return (0);
 }
-
