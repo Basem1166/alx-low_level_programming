a05d0ff17ee32ba7624adbbec414e118ab2e6628
Basem <basem_elgalfy@yahoo.com>
2023-07-03 15:10:47 -0700

    0x07

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -2,26 +2,23 @@
 
 /**
  * _strpbrk - Searches a string for any of a set of bytes
- * @s: Pointer to the string
- * @accept: Pointer to the string containing accepted bytes
+ * @s: The input string to search
+ * @accept: The string containing the bytes to search for
  *
- * Return: Pointer to the byte in s that matches one of the bytes in accept,
+ * Return: A pointer to the byte in s that matches one of the bytes in accept,
  *         or NULL if no such byte is found
  */
 char *_strpbrk(char *s, char *accept)
 {
-	while (*s)
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		char *a = accept;
-
-		while (*a)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (*s == *a)
-				return (s);
-			a++;
+			if (s[i] == accept[j])
+				return (&s[i]);
 		}
-
-		s++;
 	}
 
 	return (NULL);
