706266090315b84d88c6677a5fff44a59fcbaafb
Basem <basem_elgalfy@yahoo.com>
2023-07-10 19:34:57 -0700

    0x0B

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,6 +16,10 @@ char *str_concat(char *s1, char *s2)
 	int i;
 	int j = 0;
 
+	if (!s1)
+		s1[1] = "";
+	if(!s2)
+		s2[1] = "";
 	for (i = 0; s1[i] != '\0'; i++)
 		length++;
 	for (i = 0; s2[i] != '\0'; i++)
