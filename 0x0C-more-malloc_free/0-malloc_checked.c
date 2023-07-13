358e198960d1f4a290ef31783522bb011c77c8b1
Basem <basem_elgalfy@yahoo.com>
2023-07-13 14:28:21 -0700

    0x0C

---
+++ 0x0C-more-malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  * malloc_checked - allocates memory
+  * @b : memory to be allocated
+  * Return: pointer to memory
+  */
+void *malloc_checked(unsigned int b)
+{
+	void *m;
+	
+	m = malloc(b);
+	if (m)
+		return(m);
+	return;
+}
