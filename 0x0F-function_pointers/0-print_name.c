17281ea589a9fbffc33476e38f60d9fe5ac8b653
Basem <basem_elgalfy@yahoo.com>
2023-07-19 14:40:54 -0700

    0x0F

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+  * print_name - prints name
+  * @name : name
+  * @f : func
+  */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+		return;
+	(*f)(name);
+}
