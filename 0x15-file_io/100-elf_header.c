1084acb394bba4797e6c94188f6bf1bc3eb5b618
put username-2aaaaaaaaaadsgagagagagregeaggit config --global user.name put <basem_elgalfy@yahoo.com>
2023-08-08 13:57:35 -0700

    0x15

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -20,11 +20,13 @@ void print_elf_header_info(const Elf64_Ehdr *header)
 
 	printf("Class: %d-bit\n", header->e_ident[EI_CLASS] == ELFCLASS64 ? 64 : 32);
 
-	printf("Data: %s\n", header->e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "Unknown");
+	printf("Data: %s\n", header->e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement \
+			, little endian" : "Unknown");
 
 	printf("Version: %d\n", header->e_ident[EI_VERSION]);
 
-	printf("OS/ABI: %s\n", header->e_ident[EI_OSABI] == ELFOSABI_SYSV ? "UNIX - System V" : "Unknown");
+	printf("OS/ABI: %s\n", header->e_ident[EI_OSABI] == ELFOSABI_SYSV ? \
+			"UNIX - System V" : "Unknown");
 
 	printf("ABI Version: %d\n", header->e_ident[EI_ABIVERSION]);
 
@@ -41,21 +43,23 @@ void print_elf_header_info(const Elf64_Ehdr *header)
  */
 int main(int argc, char *argv[])
 {
+	Elf64_Ehdr header;
+	int fd;
+	ssize_t bytes_read;
+
 	if (argc != 2)
 	{
 		fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
 		return (98);
 	}
 
-	int fd = open(argv[1], O_RDONLY);
+	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 	{
 		perror("open");
 		return (98);
 	}
-
-	Elf64_Ehdr header;
-	ssize_t bytes_read = read(fd, &header, sizeof(Elf64_Ehdr));
+	bytes_read = read(fd, &header, sizeof(Elf64_Ehdr));
 	if (bytes_read != sizeof(Elf64_Ehdr))
 	{
 		fprintf(stderr, "Error reading ELF header\n");
