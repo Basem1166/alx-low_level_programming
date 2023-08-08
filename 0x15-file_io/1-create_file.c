8f4460360c8651324732c9799bb3d40eba7dcdd5
put username-2aaaaaaaaaadsgagagagagregeaggit config --global user.name put <basem_elgalfy@yahoo.com>
2023-08-08 13:41:00 -0700

    0x15

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <main.h>
+
+/**
+  * creat_file - creates a file
+  * @filename : name of file
+  * @text_content : content
+  * Return: 1 on success , -1 on fail
+  */
+
+int create_file(const char *filename, char *text_content)
+{
+	int i, file;
+
+	if (!filename)
+		return (-1);
+	if (!text_content)
+		text_content = "";
+	for (i = 0; text_content[i] != '\0'; i++)
+		;
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (file == -1)
+		return (-1);
+	write(file, text_content, i);
+
+	return (1);
+}
