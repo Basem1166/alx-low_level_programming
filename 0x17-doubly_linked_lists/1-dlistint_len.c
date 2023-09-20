2d75ff17266fac71c59ee20813c72e51ae278e87
Basem Elgalfy <basem_elgalfy@yahoo.com>
2023-09-20 08:12:18 +0300

    1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - counts all elements
+ * @h: header
+ * Return: number of nodes
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+	int count = 0;
+
+	while (h)
+	{
+		h = h->next;
+		count++;
+	}
+
+	return (count);	
+}
