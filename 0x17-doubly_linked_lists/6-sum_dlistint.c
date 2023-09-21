1367d735099ff0528faeb27b967e22a059c9a1aa
Basem Elgalfy <basem_elgalfy@yahoo.com>
2023-09-21 15:32:35 +0300

    betty fixes

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * sum_dlistint - returns the sum of all the data (n) of a dlistint_t 
+ * sum_dlistint - returns the sum of all the data (n) of a dlistint_t
  *                linked list.
  * @head: head of the list
  * Return: returns the sum
@@ -15,5 +15,5 @@ int sum_dlistint(dlistint_t *head)
 		sum += head->n;
 		head = head->next;
 	}
-	return (sum);	
+	return (sum);
 }
